fc46ac516a775f9ee25d55c5966c2b297913ec4e
Staphsynth <1337dallen@gmail.com>
2017-08-14 16:32:07 +1000

    Remove uneeded data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import tickets from './data/tickets.json'
 
 class App extends Component {
   render() {
